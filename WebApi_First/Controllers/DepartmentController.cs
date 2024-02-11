bc5c9b0d2de8f7e6067aa358d06d39648872a2d6
Gajanan24 <gs24062000@gmail.com>
2024-02-11 12:48:03 +0530

    Add project files.

---
+++ WebApi_First/Controllers/DepartmentController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_First.Models;
+
+namespace WebApi_First.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public List<Department> GetDepartments()
+        {
+            List<Department> result = new List<Department>();
+           using (var db=new organizationContext())
+            {
+                result=db.Departments.ToList();
+
+            }
+           return result;
+        }
+        [HttpGet]
+        public Department? GetDept(int id)
+        {
+            Department? d = new Department();
+            using (var db = new organizationContext())
+            {
+                d = db.Departments.Find(id);
+            }
+            return d;
+        }
+        [HttpPut]
+        public String UpdateDepartment(Department department)
+        {
+            Department? department1 = new Department();
+            using(var db = new organizationContext())
+            {
+                department1=db.Departments.Find(department.Id);
+
+                department1.Name= department.Name;
+              //  db.Departments.Add(department1);
+                db.SaveChanges();
+
+                return "Dept updated successfully";
+            }
+        }
+
+        [HttpDelete]
+        public String DeleteDepartment(int id)
+        {
+            Department? department = new Department();
+            using( var db = new organizationContext())
+            {
+               department= db.Departments.Find(id);
+                db.Remove(department);
+                db.SaveChanges();
+            }
+            return "Dept deleted successfully";
+        }
+      
+
+        [HttpPost]
+        public String InsertDepartment(Department department) 
+        {
+           // Department department1 = new Department();
+          
+            using(var db=new organizationContext()) 
+            {
+               
+                    db.Departments.Add(department);
+                    db.SaveChanges();
+
+                return "Dept added successfully";
+               
+               
+            }
+
+        }
+        
+        
+    }
+}
---
+++ WebApi_First/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_First.Models;
+
+namespace WebApi_First.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public List<Employee> GetEmployees() 
+        {
+            List<Employee> list=new List<Employee>();
+
+             using( var db=new organizationContext() )
+            {
+                    list=db.Employees.ToList();
+                    
+
+            }
+            return list;
+        }
+        [HttpPost]
+        public String InsertEmp(Employee emp)
+        {
+            using( var db=new organizationContext() )
+            {
+                db.Employees.Add( emp );
+                db.SaveChanges();
+            }
+
+            return "Employee added Successfully";
+        }
+
+        [HttpGet]
+        public Employee GetEmployee(int id) 
+        {
+            Employee emp= new Employee();  
+            
+            using( var db=new organizationContext() )
+            {
+              emp=  db.Employees.Find(id);
+
+            }
+            return emp;
+        }
+        [HttpPut]
+        public String UpdateEmployee(Employee emp)
+        {
+            Employee? employee=new Employee();
+            
+          
+            using( var db=new organizationContext())
+            {
+               employee= db.Employees.Find(emp.Id);
+                employee.Name=emp.Name;
+                employee.Password=emp.Password;
+                employee.Email=emp.Email;
+                employee.Contact=emp.Contact;
+                employee.Address=emp.Address;
+                employee.DepartmentId=emp.DepartmentId;
+                db.SaveChanges();
+
+            }
+            return "Updated Successfully";
+        }
+        [HttpDelete]
+        public String DeleteEmployee(int id) 
+        {
+            Employee? e = new Employee();
+            using ( var db=new organizationContext() ) 
+            {
+              e=  db.Employees.Find(id);   
+                if (e==null)
+                {
+                    return "Employee not found";
+                }
+                else
+                {
+                    db.Employees.Remove(e);
+                    db.SaveChanges();
+                    return "Emp deleted succesfully";
+                }
+            }
+           
+        }
+    }
+}
